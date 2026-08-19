0ba411050c6f3a5377b44990fe050b38839bb151
Shrey Saraswat <shreysaraswat1996@gmail.com>
2026-08-19 17:02:50 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const multer = require ("multer");
+const uploadFile = require ("./services/storage.service.js");
+const postModel = require ("./models/post.model");
+
+const app = express ();
+app.use (express.json()); // for raw=>json data
+
+//
+const upload = multer({storage: multer.memoryStorage()}); //for form-data=>file data
+
+//same upload single name as API "key" from postman
+app.post('/create-post', upload.single('image'), async(req, res) => {
+    // Handle post creation logic here
+
+    const result = await uploadFile(req.file.buffer);    
+
+    const post =await postModel.create({
+        image:result.url,
+        caption:req.body.caption,
+    })
+    return res.status(200).json({
+        message: "Post created successfully",
+        post
+    }); //New resource creation thats why 201 is used
+});
+
+
+//Get all posts
+app.get('/get-posts', async(req, res) =>{
+    const posts = await postModel.find(); //postModel is used to get all the posts from database
+    return res.status(200).json({
+        message: "Posts retrieved successfully",
+        posts
+    });
+});
+
+module.exports = app;
---
+++ steps.md
@@ -0,0 +1,7 @@
+* Cloud Storage Provider => We will give file it provide URL for it
+Ex. ImageKit.io, Cloudinary, amazon S3 
+* Use .json() for raw data in POST API
+* Use multer for form data in POST API => const upload = multer({storage: multer.memoryStorage()});
+* Upload Buffer data of file on Imagekit.io
+=> Imagekit Docs => Select Nodejs
+*npm i dotenv for .env usage
